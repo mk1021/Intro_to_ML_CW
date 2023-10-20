97c31a3fc36a36c5a12552b0308dbe9f8c9e9fd4
Vidi-M <116262063+Vidi-M@users.noreply.github.com>
2023-10-20 14:39:22 +0100

    Code to visulise 3D scatter plots of features

--- data/visualise.py
+++ data/visualise.py
@@ -1,24 +1,47 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-# def read_dataset(filepath):
-  
-#     x = []
-#     room = []
-#     for line in open(filepath):
-#         if line.strip() != "": # handle empty rows in file
-#             row = line.strip().split(" ")
-#             x.append(list(map(float, row[:-1]))) 
-#             room.append(row[-1])
-    
-#     x = np.array(x)
-#     [classes, y] = np.unique(room, return_inverse=True) 
 
-#     x = np.array(x)
-#     y = np.array(y)
-#     return (x, y, classes)
+def read_dataset(filepath):
+    x = []
+    room = []
+    for line in open(filepath):
+        if line.strip() != "":  # handle empty rows in file
+            row = line.strip().split("\t")
+            x.append(list(map(float, row[:-1])))
+            room.append(row[-1])
 
-# (x, y, classes) = read_dataset("data/clean_dataset.txt")
- 
-for line in open('clean_dataset.txt'):
-    print(line)
-    
+    [classes, y] = np.unique(room, return_inverse=True)
+
+    x = np.array(x)
+    y = np.array(y)
+
+    return (x, y, classes)
+
+
+(x, y, classes) = read_dataset("clean_dataset.txt")
+
+for i in range(0, 4):
+    print("Class " + str(i))
+    print(x[y == i].min(axis=0))
+    print(x[y == i].max(axis=0))
+    print(np.median(x[y == i], axis=0))
+
+# feature_names = ["WIFI_1", "WIFI_2", "WIFI_3", "WIFI_4", "WIFI_5", "WIFI_6",
+#                  "WIFI_7"]
+#
+# # plt.figure()
+# # ax = plt.axes(projection='3d')
+# #plt.scatter(x[:, 0], x[:, 1], x[:, 2], c=y, cmap=plt.cm.Set1, edgecolor='k')
+# # plt.xlabel(feature_names[0])
+# # plt.ylabel(feature_names[1])
+# # plt.show()
+
+
+fig = plt.figure(figsize = (10,10))
+ax = plt.axes(projection='3d')
+ax.grid()
+
+ax.scatter(x[:, 2], x[:, 3], x[:, 4], c=y, cmap=plt.cm.Set1, edgecolor='k')
+ax.set_title('3D Scatter Plot')
+plt.show()
