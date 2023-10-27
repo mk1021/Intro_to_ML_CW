fea3fba5394073a14829e4764b98e3cd7dcfe68b
mk1021 <93614234+mk1021@users.noreply.github.com>
2023-10-27 23:55:16 +0100

    accuracy, precision, recall, f1

--- decision_tree.py
+++ decision_tree.py
@@ -127,3 +127,93 @@ def find_split(training_dataset):
 def split_dataset(training_dataset, split):
     # Implement your logic here
     pass
+
+
+def confusion_matrix(true_labels, predicted_labels):
+    # HINT: you should get a single 4x4 matrix
+
+    # [ TP  FN  |  FP  TN ] ?
+
+    class_labels = np.unique(np.concatenate((true_labels, predicted_labels)), dtype=np.int)
+    confusion = np.zeros()
+
+    for i in range(len(true_labels)):
+        true_label = true_labels[i]
+        predicted_label = predicted_labels[i]
+
+        # Find indices in class_labels for both the true and predicted labels
+        true_index = np.where(class_labels == true_label)[0]
+        predicted_index = np.where(class_labels == predicted_label)[0]
+
+        # Update the corresponding cell in the confusion matrix
+        confusion[true_index, predicted_index] += 1
+
+    return confusion
+
+
+def accuracy(confusion):
+    # (TP + TN)/(TP + TN + FP + FN)
+    # HINT: you can derive the metrics directly from the previously computed confusion matrix
+
+    if np.sum(confusion) > 0:
+        return np.trace(confusion) / np.sum(confusion)
+    else:
+        return 0
+
+
+def precision_rate(true_labels, predicted_labels):
+    # (TP)/(TP + FP)
+
+    unique_class_labels = np.unique(np.concatenate(true_labels, predicted_labels))
+    precision = np.zeros(len(unique_class_labels))
+
+    for (c, label) in enumerate(unique_class_labels):
+        tp = np.sum((true_labels == label) & (predicted_labels == label))
+        fp = np.sum((true_labels != label) & (predicted_labels == label))
+
+        if (tp + fp) > 0:
+            precision[c] = tp / (tp + fp)
+
+    return precision
+
+
+def recall_rate(true_labels, predicted_labels):
+    # (TP)/(TP + FN)
+    # recall rate per class using the eq
+
+    unique_class_labels = np.unique(np.concatenate(true_labels, predicted_labels))
+    recall = np.zeros(len(unique_class_labels))
+
+    for (c, label) in enumerate(unique_class_labels):
+        tp = np.sum((true_labels == label) & (predicted_labels == label))
+        fn = np.sum((true_labels == label) & (predicted_labels != label))
+
+        if (tp + fp) > 0:
+            recall[c] = tp / (tp + fn)
+        else:
+            recall[c] = 0.0
+
+    return recall
+
+
+def f1_score(precisions, recalls):
+    # 2(precision)(recall)/(precision + recall)
+
+    assert len(precisions) == len(recalls)
+
+    f = np.zeros(len(precisions), )
+
+    for i in range(len(precisions)):
+        current_p = precisions[i]
+        current_r = recalls[i]
+
+        if (current_p + current_r) > 0.0:
+            f_one = (2*current_p*current_r)/(current_p + current_r)
+        else:
+            f_one = 0.0
+
+        f[i] = f_one
+
+    return f
+
+
