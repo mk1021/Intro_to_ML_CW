2a56d44a24763101c7a43314e0ad91966195d8f7
mk1021 <93614234+mk1021@users.noreply.github.com>
2023-10-31 15:55:53 +0000

    cross_validation file edits

--- cross_validation.py
+++ cross_validation.py
@@ -2,59 +2,7 @@ import numpy as np
 from numpy.random import default_rng
 
 from evaluate import accuracy
-
-
-class KNNClassifier:
-    def __init__(self, k=10):
-        """ K-NN Classifier.
-
-        Args:
-            k (int): Number of nearest neighbours. Defaults to 5.
-        """
-        self.k = k
-        self.x = np.array([])
-        self.y = np.array([])
-
-    def fit(self, x, y):
-        """ Fit the training data to the classifier.
-
-        Args:
-            x (np.ndarray): Instances, numpy array with shape (N,K)
-            y (np.ndarray): Class labels, numpy array with shape (N,)
-        """
-        self.x = x
-        self.y = y
-
-    def predict(self, x):
-        """ Perform prediction given some examples.
-
-        Args:
-            x (np.ndarray): Instances, numpy array with shape (N,K)
-
-        Returns:
-            y (np.ndarray): Predicted class labels, numpy array with shape (N,)
-        """ 
-
-        # just to make sure that we have enough training examples!
-        k = min([self.k, len(self.x)])
-
-        y = np.zeros((len(x), ), dtype=self.y.dtype)       
-        for (i, instance) in enumerate(x):
-            distances = np.sqrt(np.sum((instance-self.x)**2, axis=1))
-            sorted_indices = np.argsort(distances)
-            sorted_indices = sorted_indices[:k]
-            
-            # Assign to the majority class label (the mode)
-            unique_labels, freq = np.unique(self.y[sorted_indices], return_counts=True)
-            y[i] = unique_labels[freq.argmax()]
-
-        return y
-
-
-knn_classifier = KNNClassifier(k=1) # we'll do one nearest neighbour
-knn_classifier.fit(x_train, y_train)
-knn_predictions = knn_classifier.predict(x_test)
-print(knn_predictions)
+from decision_tree import decision_tree_learning
 
 
 def k_fold_split(n_splits, n_instances, random_generator=default_rng()):
@@ -102,39 +50,35 @@ def train_test_k_fold(n_folds, n_instances, random_generator=default_rng()):
     for k in range(n_folds):
         # pick k as test
         test_indices = split_indices[k]
-
-        # combine remaining splits as train
-        # this solution is fancy and worked for me
-        # feel free to use a more verbose solution that's more readable
         train_indices = np.hstack(split_indices[:k] + split_indices[k+1:])
-
         folds.append([train_indices, test_indices])
 
     return folds
 
 
-# to test your function (30 instances, 4 fold)
+# to test your function (2000 instances, 10 fold)
 for (train_indices, test_indices) in train_test_k_fold(10, 2000, rg):
     print("train: ", train_indices)
     print("test: ", test_indices)
     print()
 
 
-def retrain(n_folds=10, x, y):
+def retrain(n_folds=10, training_dataset):
+
     accuracies = np.zeros((n_folds, ))
     for i, (train_indices, test_indices) in enumerate(train_test_k_fold(n_folds, len(x), rg)):
         # get the dataset from the correct splits
-        x_train = x[train_indices, :]
-        y_train = y[train_indices]
-        x_test = x[test_indices, :]
-        y_test = y[test_indices]
+        train = training_dataset[train_indices, :]
+        test = training_dataset[test_indices, :]
+        # x_train = x[train_indices, :]
+        # y_train = y[train_indices]
+        # x_test = x[test_indices, :]
+        # y_test = y[test_indices]
 
-        # Train the KNN (we'll use one nearest neighbour)
-        knn_classifier = KNNClassifier(k=1)
-        knn_classifier.fit(x_train, y_train)
-        predictions = knn_classifier.predict(x_test)
-        acc = accuracy(y_test, predictions)
-        accuracies[i] = acc
+        classifier = decision_tree_learning(k=1)
+        classifier.fit(train[:, -1], train[-1])
+        predictions = knn_classifier.predict(test[:, -1])
+        accuracies[i] = accuracy(test[-1], predictions)
 
         return accuracies.mean()
 
