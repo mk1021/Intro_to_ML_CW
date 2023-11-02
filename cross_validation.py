4ac7bbc4c5755bbc8160bc3bfd49ab02fc4f9896
mk1021 <93614234+mk1021@users.noreply.github.com>
2023-11-02 20:28:17 +0000

    removed decision_tree_predict

--- cross_validation.py
+++ cross_validation.py
@@ -3,6 +3,7 @@ from numpy.random import default_rng
 
 from evaluate import accuracy
 from decision_tree import decision_tree_learning
+from decision_tree import predict
 
 
 def k_fold_split(n_splits, n_instances, random_generator=default_rng()):
@@ -63,66 +64,22 @@ for (train_indices, test_indices) in train_test_k_fold(10, 2000, rg):
     print()
 
 
-def decision_tree_predict(current_node, x, training_dataset):
-    # Check if the current node is a leaf node
-    trained_dataset = decision_tree_learning(training_dataset, depth)
-    current_node = trained_dataset
-
-    if current_node.is_leaf:
-        # Determine which child node to follow based on the feature value of the current instance
-        feature_index = current_node.feature_index
-        feature_value = x[feature_index]
-
-        if feature_value <= current_node.threshold:
-            return decision_tree_predict(current_node.left_child, x)
-        else:
-            return decision_tree_predict(current_node.right_child, x)
-
-    predicted_label = current_node.majority_class
-
-#
-# def decision_tree_predict(training_dataset, x, depth):
-#     """
-#         Predict the class label for an instance using a decision tree.
-#
-#         Args:
-#         tree: The trained decision tree.
-#         instance: A numpy array containing the features of the instance.
-#
-#         Returns:
-#         predicted_label: The predicted class label.
-#         """
-#     # Start at the root of the decision tree
-#     trained_dataset = decision_tree_learning(training_dataset, depth)
-#     current_node = trained_dataset
-#
-#     # Traverse the decision tree until we reach a leaf node
-#     while not current_node.is_leaf:
-#         # Determine which child node to follow based on the feature value of the current instance
-#         feature_index = current_node.feature_index
-#         feature_value = x[feature_index]
-#         if feature_value <= current_node.threshold:
-#             current_node = current_node.left_child
-#         else:
-#             current_node = current_node.right_child
-#
-#     # The predicted class label is the majority class in the leaf node
-#     predicted_label = current_node.majority_class
-#     return predicted_label
-
-
 def retrain(n_folds=10, training_dataset):
 
     accuracies = np.zeros((n_folds, ))
     for i, (train_indices, test_indices) in enumerate(train_test_k_fold(n_folds, len(training_dataset), rg)):
         # get the dataset from the correct splits
-        train_data = training_dataset[train_indices, :]
-        train_labels = training_dataset[train_indices]
-        test_data = training_dataset[test_indices, :]
-        test_labels = training_dataset[test_indices]
+        # train_data = training_dataset[train_indices, :]
+        # train_labels = training_dataset[train_indices]
+        # test_data = training_dataset[test_indices, :]
+        # test_labels = training_dataset[test_indices]
+
+        train = training_dataset[train_indices, :]
+        test = training_dataset[test_indices, :]
 
         # DECISION TREE PREDICT
-        classifier = decision_tree_learning(np.stack(train_data, train_labels), depth=1)
+        # classifier = decision_tree_learning(np.stack(train_data, train_labels), depth=1)
+        classifier = decision_tree_learning(train, depth=1)
 
         # Initialize a list to store predictions
         predictions = []
@@ -130,7 +87,7 @@ def retrain(n_folds=10, training_dataset):
         # Iterate through the test instances and make predictions directly
         for instance in test:
             # Make a prediction for the current test instance using your classifier & append it to a list of predictions
-            predicted_label = decision_tree_predict(train, instance[:-1])  # Assuming the last column is the label
+            predicted_label = predict(train, instance[:-1])  # Assuming the last column is the label
             predictions.append(predicted_label)
 
         accuracies[i] = accuracy(test[-1], predictions)
