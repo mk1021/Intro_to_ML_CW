f3fc721812384fced1b57e4553f18763bd115db4
Vidi-M <116262063+Vidi-M@users.noreply.github.com>
2023-11-03 11:57:58 +0000

    Finished evaluate.py to print out all the calculations as the end + tested and fixed errors/bugs

--- decision_tree.py
+++ decision_tree.py
@@ -125,41 +125,6 @@ def predict(tree, x):
 
         predictions.append(branch['leaf'])
 
-    return predictions
+    return np.array(predictions)
 
 
-# MAIN:
-# ----------------------------------------------------------------------
-
-# Loading the data from the text file
-data_clean = np.loadtxt('data/clean_dataset.txt')
-data_noisy = np.loadtxt('data/noisy_dataset.txt')
-
-# Printing the loaded data
-# print(data_clean)
-
-# For quick testing
-# print(find_split(data_clean))
-# print(split_dataset(data_clean, find_split(data_clean))[1])
-# print(decision_tree_learning(data_clean, 0))
-
-
-
-# # Call the decision tree plot function
-test_total = round(len(data_clean) * 0.2)
-rg = default_rng(60012)
-rand_indexes = rg.permutation(len(data_noisy))
-test = data_clean[rand_indexes[:test_total]]
-train = data_clean[rand_indexes[test_total:]]
-
-decision_tree = decision_tree_learning(train, 0)[0]
-maxdepth = decision_tree_learning(train, 0)[1]
-y_prediction = predict(decision_tree, test[:, :-1])
-y_gold = test[:, -1]
-
-print(decision_tree)
-print(np.sum(y_gold == y_prediction) / len(y_gold))
-
-generate_tree(decision_tree, maxdepth, "tree.png")
-
-plt.show()
--- evaluate.py
+++ evaluate.py
@@ -1,4 +1,7 @@
 import numpy as np
+from numpy.random import default_rng
+
+from decision_tree import decision_tree_learning, predict
 
 
 def confusion_matrix(true_labels, predicted_labels):
@@ -6,8 +9,7 @@ def confusion_matrix(true_labels, predicted_labels):
     # [ TP  FN  |  FP  TN ] ?
 
     # Define class labels
-    class_labels = np.unique(np.concatenate((true_labels, predicted_labels)),
-                             dtype=np.int)
+    class_labels = np.unique(np.concatenate((true_labels, predicted_labels)))
 
     # Initialize the confusion matrix
     confusion = np.zeros((len(class_labels), len(class_labels)), dtype=int)
@@ -44,25 +46,29 @@ def accuracy(confusion):
 def precision_rate(confusion):
     # (TP)/(TP + FP)
 
-    precision = []
+    precisions = []
     for i in range(len(confusion)):
         tp = confusion[i, i]
         col_sum = np.sum(confusion[:, i])
-        precision.append(tp / col_sum)
+        precisions.append(tp / col_sum)
 
-    return np.array(precision)
+    macro_p = np.mean(precisions)
+
+    return np.array(precisions), macro_p
 
 
 def recall_rate(confusion):
     # (TP)/(TP + FN))
 
-    recall = []
+    recalls = []
     for i in range(len(confusion)):
         tp = confusion[i, i]
         row_sum = np.sum(confusion[i, :])
-        recall.append(tp / row_sum)
+        recalls.append(tp / row_sum)
 
-    return np.array(recall)
+    macro_r = np.mean(recalls)
+
+    return np.array(recalls), macro_r
 
 
 def f1_score(precisions, recalls):
@@ -75,4 +81,49 @@ def f1_score(precisions, recalls):
 
     f1 = np.divide(2 * np.multiply(precisions, recalls), precisions + recalls)
 
-    return f1
+    macro_f = np.mean(f1)
+
+    return f1, macro_f
+
+
+def evaluate(true_labels, predicted_labels):
+    confusion = confusion_matrix(true_labels, predicted_labels)
+    acc = accuracy(confusion)
+    class_precisions, macro_precision = precision_rate(confusion)
+    class_recalls, macro_recall = recall_rate(confusion)
+    f1_scores, macro_f1 = f1_score(class_precisions, class_recalls)
+
+    print(f"Confusion Matrix: \n {confusion}")
+    print()
+    print(f"Accuracy: {acc}")
+    print()
+    print("Precision: ")
+    for i in range(len(class_precisions)):
+        print(f"\tClass {i}: {class_precisions[i]}")
+    print(f"Macro-averaged Precision: {macro_precision}")
+    print()
+    print("Recall: ")
+    for i in range(len(class_recalls)):
+        print(f"\tClass {i}: {class_recalls[i]}")
+    print(f"Macro-averaged Recall: {macro_recall}")
+    print()
+    print("F1 Scores: ")
+    for i in range(len(f1_scores)):
+        print(f"\tClass {i}: {f1_scores[i]}")
+    print(f"Macro-averaged F1 Score: {macro_f1}")
+
+
+data_clean = np.loadtxt('data/noisy_dataset.txt')
+
+test_total = round(len(data_clean) * 0.2)
+rg = default_rng(60012)
+rand_indexes = rg.permutation(len(data_clean))
+test = data_clean[rand_indexes[:test_total]]
+train = data_clean[rand_indexes[test_total:]]
+
+decision_tree = decision_tree_learning(train, 0)[0]
+max_depth = decision_tree_learning(train, 0)[1]
+y_prediction = predict(decision_tree, test[:, :-1])
+y_gold = test[:, -1]
+
+print(evaluate(y_gold, y_prediction))
