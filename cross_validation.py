a5f475ff8723bbe8a39b5f135e143ab320ad98ba
mk1021 <93614234+mk1021@users.noreply.github.com>
2023-11-03 09:41:01 +0000

    finished cross validation functions

--- cross_validation.py
+++ cross_validation.py
@@ -2,8 +2,8 @@ import numpy as np
 from numpy.random import default_rng
 
 from evaluate import accuracy
-from decision_tree import decision_tree_learning
 from decision_tree import predict
+from decision_tree import data_clean
 
 
 def k_fold_split(n_splits, n_instances, random_generator=default_rng()):
@@ -19,10 +19,10 @@ def k_fold_split(n_splits, n_instances, random_generator=default_rng()):
             numpy array giving the indices of the instances in that split.
     """
 
-    # generate a random permutation of indices from 0 to n_instances
+    # Generate a random permutation of indices from 0 to n_instances
     shuffled_indices = random_generator.permutation(n_instances)
 
-    # split shuffled indices into almost equal sized splits
+    # Split shuffled indices into almost equal sized splits
     split_indices = np.array_split(shuffled_indices, n_splits)
 
     return split_indices
@@ -44,7 +44,7 @@ def train_test_k_fold(n_folds, n_instances, random_generator=default_rng()):
             numpy array containing the test indices.
     """
 
-    # split the dataset into k splits
+    # Split the dataset into k splits
     split_indices = k_fold_split(n_folds, n_instances, random_generator)
 
     folds = []
@@ -57,45 +57,36 @@ def train_test_k_fold(n_folds, n_instances, random_generator=default_rng()):
     return folds
 
 
-# to test your function (2000 instances, 10 fold)
+# To test your function (2000 instances, 10 fold)
 for (train_indices, test_indices) in train_test_k_fold(10, 2000, rg):
     print("train: ", train_indices)
     print("test: ", test_indices)
     print()
 
 
-def retrain(n_folds=10, training_dataset):
+# Separate data_clean into data and labels
+data = data_clean[:, :-1]
+labels = data_clean[-1]
 
-    accuracies = np.zeros((n_folds, ))
-    for i, (train_indices, test_indices) in enumerate(train_test_k_fold(n_folds, len(training_dataset), rg)):
-        # get the dataset from the correct splits
-        # train_data = training_dataset[train_indices, :]
-        # train_labels = training_dataset[train_indices]
-        # test_data = training_dataset[test_indices, :]
-        # test_labels = training_dataset[test_indices]
+# Computing the accuracy for each fold
+n_folds = 10
+accuracies = np.zeros((n_folds, ))
+for i, (train_indices, test_indices) in enumerate(train_test_k_fold(n_folds, len(data), rg)):
 
-        train = training_dataset[train_indices, :]
-        test = training_dataset[test_indices, :]
+    # Get the dataset from the correct splits
+    train_data = data[train_indices, :]
+    train_labels = labels[train_indices]
+    test_data = data[test_indices, :]
+    test_labels = labels[test_indices]
 
-        # DECISION TREE PREDICT
-        # classifier = decision_tree_learning(np.stack(train_data, train_labels), depth=1)
-        classifier = decision_tree_learning(train, depth=1)
+    # Initialize a list to store predictions
+    predictions = []
 
-        # Initialize a list to store predictions
-        predictions = []
+    # Iterate through the test instances and make predictions directly
+    predictions = predict(test_data)
+    accuracies[i] = accuracy(test_labels, predictions)
 
-        # Iterate through the test instances and make predictions directly
-        for instance in test:
-            # Make a prediction for the current test instance using your classifier & append it to a list of predictions
-            predicted_label = predict(train, instance[:-1])  # Assuming the last column is the label
-            predictions.append(predicted_label)
-
-        accuracies[i] = accuracy(test[-1], predictions)
-
-
-    return accuracies.mean()
-
-
-# print(accuracies)
-# print(accuracies.mean())
-# we don't need Standard Deviation - print(accuracies.std())
+# print out the mean accuracy to find the best model?
+print(accuracies)
+print(accuracies.mean())
+print(accuracies.std())
